932da72ce30a21df97e7828f263c0acbf55deead
Rahul12347 <2019rahul.maurya@ves.ac.in>
2024-08-31 16:11:20 +0530

    first commit

---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,25 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import { MenuItem } from "../types";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+
+type Props = {
+  menuItem: MenuItem;
+  addToCart: () => void;
+};
+
+const MenuItem = ({ menuItem, addToCart }: Props) => {
+  const exchangeRateGBPtoINR = 1;
+  return (
+    <Card className="cursor-pointer" onClick={addToCart}>
+      <CardHeader>
+        <CardTitle>{menuItem.name}</CardTitle>
+      </CardHeader>
+      <CardContent className="font-bold">
+       ₹{(menuItem.price / 100 * exchangeRateGBPtoINR).toFixed(2)}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default MenuItem;
